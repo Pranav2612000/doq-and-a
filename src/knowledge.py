0e3974bae95a4b42084bfdb121b71b493d07a19f
Pranav2612000 <pranav2000joglekar@gmail.com>
2024-06-30 15:50:09 +0530

    fix: don't delete db folder

--- src/knowledge.py
+++ src/knowledge.py
@@ -5,7 +5,6 @@ from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 import openai
 import os
-import shutil
 
 load_dotenv()
 openai.api_key = os.environ['OPENAI_API_KEY']
@@ -47,9 +46,6 @@ def split_text(documents):
     return chunks;
 
 def save(chunks):
-    if os.path.exists(CHROMA_PATH):
-        shutil.rmtree(CHROMA_PATH)
-
     db = Chroma.from_documents(
         chunks, OpenAIEmbeddings(), persist_directory=CHROMA_PATH
     )
