f9f828d07e4f88bd77317d1eff82b6c1d2252635
Pranav2612000 <pranav2000joglekar@gmail.com>
2024-06-30 13:04:49 +0530

    feat: create func to get responses to queries

---
+++ src/query.py
@@ -0,0 +1,47 @@
+import argparse
+
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
+from langchain_openai import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from dotenv import load_dotenv
+import os
+import openai
+
+load_dotenv()
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+CHROMA_PATH = os.environ['CHROMA_PATH']
+
+PROMPT_TEMPLATE = """
+Answer the question based only on the following context:
+
+{context}
+
+---
+
+Answer the question based on the above context: {question}
+"""
+
+def get_answer(query):
+    embedding_function = OpenAIEmbeddings()
+    db = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding_function)
+
+    results = db.similarity_search_with_relevance_scores(query, k = 3)
+    if len(results) == 0 or results[0][1] < 0.7:
+        print("Unable to find matching results.")
+        return
+
+    context_text = "\n\n\n".join([doc.page_content for doc, _score in results])
+    prompt_template = ChatPromptTemplate.from_template(PROMPT_TEMPLATE)
+    prompt = prompt_template.format(context=context_text, question=query)
+    print(prompt)
+
+    model = ChatOpenAI()
+    response_text = model.predict(prompt)
+
+    sources = [doc.metadata.get("source", None) for doc, _score in results]
+    return { "response": response_text, "sources": sources }
+
+x = get_answer("Why has Mr Pranav Joglekar expressed interest in travelling to Germany")
+print(x)
