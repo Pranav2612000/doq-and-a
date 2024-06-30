db6281f3ce2eac8cd7370cf146c95b59c38fa367
Pranav2612000 <pranav2000joglekar@gmail.com>
2024-06-30 12:21:04 +0530

    feat: download and save documents locally

--- src/index.py
+++ src/index.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import drive
 
 app = Flask(__name__)
 
@@ -7,6 +8,9 @@ def get_form_response():
     body = request.get_json()
     question = body["Ask your question"]
     documents = body["Upload your documents"]
+    for documentId in documents:
+        print(f"downloading {documentId}")
+        drive.download_pdf(documentId)
     return { "success": True }
 
 app.run()
