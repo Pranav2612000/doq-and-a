abb0fd00de44ef8e9d7f7eb1b8defc8b11cde1c5
Pranav2612000 <pranav2000joglekar@gmail.com>
2024-06-30 15:50:26 +0530

    feat: send email to user with response

--- src/index.py
+++ src/index.py
@@ -6,6 +6,7 @@ import os
 import drive
 import knowledge
 import query
+import emailer
 
 DATA_PATH = os.environ['DATA_PATH']
 CHROMA_PATH = os.environ['CHROMA_PATH']
@@ -24,6 +25,7 @@ def get_form_response():
     body = request.get_json()
     question = body["Ask your question"]
     documents = body["Upload your documents"]
+    email = body["email"]
 
     # Delete previous documents if any exist
     clear_directory(DATA_PATH)
@@ -35,7 +37,16 @@ def get_form_response():
     knowledge.generate_knowledge()
 
     response = query.get_answer(question)
-    print(response)
+    if response:
+        message = f"""\
+        Subject: DoQ-and-A | Answers Ready
+
+        Q: {question}
+        A: {response["response"]}
+        Sources: {response["sources"]}
+        """
+        emailer.send_email(email, message)
+        print('Email Sent')
 
     # Delete these saved documents and db
     clear_directory(DATA_PATH)
